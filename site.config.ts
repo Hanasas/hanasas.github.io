f6db0463f3a03a4c66819e36f948e2267f468f78
GouceQ <gouceq@gmail.com>
2025-05-30 04:40:56 +0800

    initial commit

---
+++ site.config.ts
@@ -0,0 +1,64 @@
+import { defineSiteConfig } from 'valaxy'
+
+const url = 'https://hanasas.github.io/'
+
+export default defineSiteConfig({
+  url: url,
+  lang: 'zh-CN',
+  title: '「迷い星のうた」',
+  author: {
+    name: 'Hanasas',
+    avatar: url + 'images/avatar.jpg',
+  },
+  subtitle: '',
+  description: 'ACGN/Vtuber热爱者，学艺不精的独立设计师/开发者',
+  social: [
+    {
+      name: 'RSS',
+      link: '/atom.xml',
+      icon: 'i-ri-rss-line',
+      color: 'orange',
+    },
+    {
+      name: 'QQ',
+      link: 'https://qm.qq.com/q/oJxzGdDksg',
+      icon: 'i-ri-qq-line',
+      color: '#12B7F5',
+    },
+    {
+      name: 'GitHub',
+      link: 'https://github.com/Hanasas',
+      icon: 'i-ri-github-line',
+      color: '#6e5494',
+    },
+    {
+      name: '网易云音乐',
+      link: 'https://music.163.com/#/user/home?id=1349530851',
+      icon: 'i-ri-netease-cloud-music-line',
+      color: '#C20C0C',
+    },
+    {
+      name: '哔哩哔哩',
+      link: 'https://space.bilibili.com/215505279',
+      icon: 'i-ri-bilibili-line',
+      color: '#FF8EB3',
+    },
+    {
+      name: 'E-Mail',
+      link: 'gouchengouceq@163.com',
+      icon: 'i-ri-mail-line',
+      color: '#8E71C1',
+    },
+  ],
+
+  search: {
+    enable: false,
+  },
+
+  sponsor: {
+    enable: false,
+    title: '我很可爱，请给我钱！',
+    methods: [
+    ],
+  },
+})
---
+++ valaxy.config.ts
@@ -0,0 +1,48 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '迷い星のうた',
+    },
+
+    pages: [
+      {
+        name: '我的小伙伴们',
+        url: '/links/',
+        icon: 'i-ri-genderless-line',
+        color: 'dodgerblue',
+      },
+      {
+        name: '喜欢的女孩子',
+        url: '/girls/',
+        icon: 'i-ri-women-line',
+        color: 'hotpink',
+      },
+    ],
+
+    footer: {
+      since: 2021,
+      beian: {
+        enable: false,
+        icp: '',
+      },
+    },
+  },
+
+  unocss: { safelist },
+})
